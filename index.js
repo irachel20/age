f33c505633b03d0454c39e45fafc27d2a2ecc2e7
unknown <rahiljaafari81@gmail.com>
2022-07-01 17:14:17 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const userName = `rahil`;
+const birthDay = 2002;
+
+const rahilsCurrentAge = 2022 - birthDay;
+
+console.log(rahilsCurrentAge);
+
+  const ageAfterFiftyYears = rahilsCurrentAge + 50;
+  console.log(ageAfterFiftyYears);
+
+if ( rahilsCurrentAge <= 20 && rahilsCurrentAge >= 18 ) {
+  console.log("you can visit the site")
+}
+else {
+  console.log("the site is forbidden for you")
+}
